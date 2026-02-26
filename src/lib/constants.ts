470594ff502e04371a854d3b3d096811fc6ab8e4
Taha Amir <tahashah61@gmail.com>
2026-02-26 15:52:16 -0600

    Updated Nav Links & Changed Color Accent

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 export const NAV_LINKS = [
   { label: "About", href: "#about" },
+  { label: "Experience", href: "#experience" },
   { label: "Projects", href: "#projects" },
-  { label: "Skills", href: "#skills" },
   { label: "Contact", href: "#contact" },
 ];
 
